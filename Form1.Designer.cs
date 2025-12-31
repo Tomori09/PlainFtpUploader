1387afa95ef38684c36b4625817a319f60760c22
sonnett09 <waveenhermosura09@gmail.com>
2025-12-31 12:17:47 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace FtpUploader
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtFtpUrl;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtLocalPath;
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnDownload;
+
+        private System.Windows.Forms.Label lblFtpUrl;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblLocalPath;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtFtpUrl = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtLocalPath = new System.Windows.Forms.TextBox();
+
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+
+            this.lblFtpUrl = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblLocalPath = new System.Windows.Forms.Label();
+
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+
+            this.SuspendLayout();
+
+            // 
+            // lblFtpUrl
+            // 
+            this.lblFtpUrl.AutoSize = true;
+            this.lblFtpUrl.Location = new System.Drawing.Point(20, 20);
+            this.lblFtpUrl.Name = "lblFtpUrl";
+            this.lblFtpUrl.Size = new System.Drawing.Size(54, 15);
+            this.lblFtpUrl.Text = "FTP URL";
+
+            // 
+            // txtFtpUrl
+            // 
+            this.txtFtpUrl.Location = new System.Drawing.Point(120, 17);
+            this.txtFtpUrl.Name = "txtFtpUrl";
+            this.txtFtpUrl.Size = new System.Drawing.Size(350, 23);
+
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(20, 55);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(63, 15);
+            this.lblUsername.Text = "Username";
+
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(120, 52);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 23);
+
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(20, 90);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(57, 15);
+            this.lblPassword.Text = "Password";
+
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(120, 87);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 23);
+            this.txtPassword.UseSystemPasswordChar = true;
+
+            // 
+            // lblLocalPath
+            // 
+            this.lblLocalPath.AutoSize = true;
+            this.lblLocalPath.Location = new System.Drawing.Point(20, 125);
+            this.lblLocalPath.Name = "lblLocalPath";
+            this.lblLocalPath.Size = new System.Drawing.Size(65, 15);
+            this.lblLocalPath.Text = "Local File";
+
+            // 
+            // txtLocalPath
+            // 
+            this.txtLocalPath.Location = new System.Drawing.Point(120, 122);
+            this.txtLocalPath.Name = "txtLocalPath";
+            this.txtLocalPath.Size = new System.Drawing.Size(260, 23);
+
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(390, 121);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(80, 25);
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(120, 165);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 30);
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(230, 165);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(100, 30);
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 220);
+            this.Controls.Add(this.lblFtpUrl);
+            this.Controls.Add(this.txtFtpUrl);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblLocalPath);
+            this.Controls.Add(this.txtLocalPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.Text = "FTP Upload / Download";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace FtpUploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Browse local file
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtLocalPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        // UPLOAD to FileZilla Server
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ftpUrl = txtFtpUrl.Text.Trim();  // Must include filename
+                string localFile = txtLocalPath.Text;
+
+                if (string.IsNullOrEmpty(ftpUrl) || string.IsNullOrEmpty(localFile))
+                {
+                    MessageBox.Show("Please provide FTP URL and local file path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential(txtUsername.Text, txtPassword.Text);
+
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.KeepAlive = false;
+
+                byte[] fileBytes = File.ReadAllBytes(localFile);
+                request.ContentLength = fileBytes.Length;
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(fileBytes, 0, fileBytes.Length);
+                }
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    MessageBox.Show("Upload complete!\n" + response.StatusDescription, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("FTP Upload Error:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // DOWNLOAD from FileZilla Server
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtFtpUrl.Text))
+                {
+                    MessageBox.Show("Please provide FTP URL including filename.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                    return;
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(txtFtpUrl.Text.Trim());
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = new NetworkCredential(txtUsername.Text, txtPassword.Text);
+
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.KeepAlive = false;
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                using (Stream ftpStream = response.GetResponseStream())
+                using (FileStream fileStream = new FileStream(saveFileDialog1.FileName, FileMode.Create))
+                {
+                    ftpStream.CopyTo(fileStream);
+                }
+
+                MessageBox.Show("Download complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("FTP Download Error:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
